df04d76bc5e531cbaad938f479dc40b5cc8e46d4
Leung Ho Yin <50589491+hoyinleung@users.noreply.github.com>
2022-02-03 18:26:37 +0000

    change

--- src/ProductList.js
+++ src/ProductList.js
@@ -22,7 +22,7 @@ export default function ProductList() {
     return (
         //React Fragment簡寫
         <>
-            <Title mainTitle="React入門水果店" />
+            <Title mainTitle="React入門水果店 第二版" />
             
             <div className="container">
                 {
